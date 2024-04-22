479dae9d383886ba14bd529a1e99ee67a67d2e48
Sérgio Rosa <154342046+LT-Rosa@users.noreply.github.com>
2024-04-22 11:39:25 +0000

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProjLDS")]
+[assembly: AssemblyTitle("LDS-SharpCoders")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProjLDS")]
+[assembly: AssemblyProduct("LDS-SharpCoders")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
