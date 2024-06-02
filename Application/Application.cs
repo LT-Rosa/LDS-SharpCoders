3ab2efbca1d4f9dc7b90f2961b9c4f155e439660
Sérgio Rosa <154342046+LT-Rosa@users.noreply.github.com>
2024-06-02 15:36:52 +0000

    Update Application.cs

--- Application/Application.cs
+++ Application/Application.cs
@@ -37,8 +37,14 @@ namespace Application
 
         public interface IModel
         {
+<<<<<<< Updated upstream
             // Define o modelo de dados de saída
             void RecolherDadosFicheiro(List<FinancialData> datalist, List<FinancialData> dataToAnalyse); // Recolhe os dados do ficheiro
+=======
+            void RecolherDadosFicheiro(List<FinancialData> datalist, List<FinancialData> dataToAnalyse);
+            void ProcessarDadosAPI(List<FinancialData> dataList, List<FinancialData> dataToAnalyse);
+           
+>>>>>>> Stashed changes
         }
 
     }
